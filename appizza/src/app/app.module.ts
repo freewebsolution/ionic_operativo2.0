eda39a5eb232b8043069872f1caec58d0a838642
Lucio <info@freewebsolution.it>
2022-06-06 10:11:08 +0200

    Imposta il token jwt nell'intestazione con angular httpinterceptor

--- appizza/src/app/app.module.ts
+++ appizza/src/app/app.module.ts
@@ -5,16 +5,29 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NavComponent } from './shared/nav/nav.component';
 import { ToolbarComponent } from './shared/toolbar/toolbar.component';
 import { CommentiformPage } from './commenti/commentiform/commentiform.page';
+import { AuthInterceptor } from './shared/auth.interceptor';
 @NgModule({
   declarations: [AppComponent, NavComponent, ToolbarComponent, CommentiformPage],
   entryComponents: [CommentiformPage],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, FormsModule, HttpClientModule],
 
-providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+
+  providers: [
+    // {
+    //   provide: RouteReuseStrategy,
+    //   useClass: IonicRouteStrategy ,
+    //   multi: true
+    // },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {
---
+++ appizza/src/app/shared/auth.interceptor.ts
@@ -0,0 +1,20 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { HttpRequest, HttpHandler, HttpInterceptor, HttpEvent } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { TokenService } from '../services/token.service';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor(private tokenservice: TokenService) { }
+  intercept(req: HttpRequest<any>, next: HttpHandler) {
+    const accessToken = this.tokenservice.getToken();
+    req = req.clone({
+      setHeaders: {
+        Authorization: 'Bearer' + accessToken
+      }
+    });
+    return next.handle(req);
+
+  }
+}
