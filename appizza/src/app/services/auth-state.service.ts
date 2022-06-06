bf2b9b63d0f83027c91331eda008e0d38c60d3fd
Lucio <info@freewebsolution.it>
2022-06-06 09:50:33 +0200

    Auth-state implements

--- appizza/src/app/services/auth-state.service.ts
+++ appizza/src/app/services/auth-state.service.ts
@@ -1,9 +1,16 @@
+/* eslint-disable @typescript-eslint/member-ordering */
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { TokenService } from './token.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthStateService {
-
-  constructor() { }
+  private userState = new BehaviorSubject<boolean>(this.token.isLoggedIn());
+  userAuthState = this.userState.asObservable();
+  constructor(public token: TokenService) { }
+  setAuthState(value: boolean){
+    this.userState.next(value);
+  }
 }
