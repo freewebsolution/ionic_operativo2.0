d11236bb53acbd9a5c8a1fc535754ddd4d54d160
Lucio <info@freewebsolution.it>
2022-06-09 09:47:25 +0200

    Implementa validators nel form

--- appizza/src/app/signup/signup.page.ts
+++ appizza/src/app/signup/signup.page.ts
@@ -1,6 +1,7 @@
+/* eslint-disable quote-props */
 /* eslint-disable @typescript-eslint/naming-convention */
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
@@ -12,6 +13,30 @@ import { AuthService } from '../services/auth.service';
 export class SignupPage implements OnInit {
   registerForm: FormGroup;
   errors: any = null;
+  submitted = false;
+  success = '';
+  error_messages = {
+    'name': [
+      { type: 'required', message: '*Name is required.' },
+    ],
+
+    'email': [
+      { type: 'required', message: '*Email is required.' },
+      { type: 'minlength', message: '*Email length.' },
+      { type: 'maxlength', message: '*Email length.' },
+      { type: 'pattern', message: '*please enter a valid email address.' }
+    ],
+
+    'password': [
+      { type: 'required', message: '*password is required.' },
+      { type: 'minlength', message: '*password length.' },
+      { type: 'pattern', message: '*Inserisci una maiuscola,una minuscola, numero, carattere speciale ' }
+    ],
+    'password_confirmation': [
+      { type: 'required', message: '*password is required.' },
+      { type: 'minlength', message: '*password length.' }
+    ],
+  };
   constructor(
     public router: Router,
     public fb: FormBuilder,
@@ -24,7 +49,34 @@ export class SignupPage implements OnInit {
       password_confirmation: [''],
     });
   }
-  ngOnInit() { }
+  ngOnInit() {
+    this.registerForm = this.fb.group({
+      name: new FormControl('', Validators.compose([
+        Validators.required
+      ])),
+      email: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')
+      ])),
+      password: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(8),
+        Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$'),
+      ])),
+      password_confirmation: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(8),
+      ])),
+    }, {
+      validators: this.password.bind(this)
+    });
+  }
+  password(formGroup: FormGroup) {
+    const { value: password } = formGroup.get('password');
+    const { value: confirmPassword } = formGroup.get('password_confirmation');
+    return password === confirmPassword ? null : { passwordNotMatch: true };
+  }
+
   onSubmit() {
     this.authService.register(this.registerForm.value).subscribe(
       (result) => {
