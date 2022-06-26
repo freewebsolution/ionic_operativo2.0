56610e7d3e9a42219913bc49efc311fb4387f5bb
Lucio <info@freewebsolution.it>
2022-06-26 18:35:06 +0200

    Gestire i messaggi toast

--- appizza/src/app/login/login.page.ts
+++ appizza/src/app/login/login.page.ts
@@ -2,6 +2,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import { LoadingController, ToastController } from '@ionic/angular';
 import { AuthStateService } from '../services/auth-state.service';
 import { AuthService } from '../services/auth.service';
 import { TokenService } from '../services/token.service';
@@ -20,15 +21,18 @@ export class LoginPage implements OnInit {
     public fb: FormBuilder,
     public authService: AuthService,
     private token: TokenService,
-    private authState: AuthStateService
+    private authState: AuthStateService,
+    private loading: LoadingController,
+    private toast: ToastController
   ) {
     this.loginForm = this.fb.group({
       email: [],
       password: [],
     });
-    this.styleCss = {display:'none'};
+    this.styleCss = { display: 'none' };
   }
-  ngOnInit() { }
+  ngOnInit() {
+  }
   onSubmit() {
     this.styleCss = { display: 'block' };
     this.authService.login(this.loginForm.value).subscribe(
@@ -42,15 +46,30 @@ export class LoginPage implements OnInit {
         this.authState.setAuthState(true);
         this.loginForm.reset();
         this.styleCss = { display: 'none' };
-        this.router.navigate(['home']);
-          // .then(() => {
-          //   window.location.reload();
-          // });
+        this.showToast('Login avvenuto con successo');
+        setTimeout(()=>{
+          window.location.href = 'http://localhost:8100/home';
+        },4000);
       }
     );
   }
+
   // Handle response
   responseHandler(data: any) {
     this.token.handleData(data.access_token);
+  }
+  // async showLoading() {
+  //   const loader = await this.loading.create({
+  //     message: 'Caricamento in corso',
+  //     duration: 2000
+  //   });
+  //   await loader.present();
+  // }
+  async showToast(msg) {
+    const toast = await this.toast.create({
+      message: msg,
+      duration: 4000,
+    });
+    await toast.present();
   }
 }
